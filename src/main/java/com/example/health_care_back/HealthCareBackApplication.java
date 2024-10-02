9e783bea24a16ca555418584b37439410d03ac8f
K-IMjihun <kjh950901@gmail.com>
2024-10-02 12:44:31 +0900

    Initial commit

---
+++ src/main/java/com/example/health_care_back/HealthCareBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.health_care_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareBackApplication.class, args);
+	}
+
+}
