c3b1fe8148fe6ca8ed3209b4fd0d85b0f0982b6c
K-IMjihun <kjh950901@gmail.com>
2024-10-02 14:16:52 +0900

    User 회원가입 구현중

--- src/main/java/com/example/health_care_back/application/account/repository/UserRepository.java
+++ src/main/java/com/example/health_care_back/application/account/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.health_care_back.application.account.repository;
 
-public class UserRepository {
+import com.example.health_care_back.application.account.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/example/health_care_back/application/account/service/UserAnService.java
+++ src/main/java/com/example/health_care_back/application/account/service/UserAnService.java
@@ -1,6 +1,10 @@
 package com.example.health_care_back.application.account.service;
 
+import com.example.health_care_back.application.account.controller.dto.CreateUserDTO;
+import com.example.health_care_back.application.account.domain.User;
 import com.example.health_care_back.application.account.repository.UserRepository;
+import com.example.health_care_back.application.common.exception.DuplicateException;
+import com.example.health_care_back.application.common.exception.DuplicateException.DuplicateExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,21 +15,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserAnService {
 
-//    private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public boolean getExistence(CheckExistenceUserDTO dto) {
-//        return userRepository.findByEmail(dto.email()).isPresent();
-//    }
-//
-//    @Transactional
-//    public void signUp(CreateUserDTO dto) {
-//        if (userRepository.findByEmail(dto.email()).isPresent()) {
-//            throw new DuplicateException(DuplicateExceptionCode.DUPLICATE_EMAIL);
-//        }
-//
-//        String encodedPassword = passwordEncoder.encode(dto.newPassword());
-//        User user = new User(dto, encodedPassword);
-//        userRepository.save(user);
-//    }
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signUp(CreateUserDTO dto) {
+        if (userRepository.findByEmail(dto.email()).isPresent()) {
+            throw new DuplicateException(DuplicateExceptionCode.DUPLICATE_EMAIL);
+        }
+
+        String encodedPassword = passwordEncoder.encode(dto.newPassword());
+        User user = new User(dto, encodedPassword);
+        userRepository.save(user);
+    }
 }
