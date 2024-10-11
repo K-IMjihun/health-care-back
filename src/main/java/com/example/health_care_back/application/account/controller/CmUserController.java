168953c5da4c94eee4f72ab6a0ff8cecc303f062
K-IMjihun <kjh950901@gmail.com>
2024-10-11 20:10:53 +0900

    비밀번호 확인기능 추가

--- src/main/java/com/example/health_care_back/application/account/controller/CmUserController.java
+++ src/main/java/com/example/health_care_back/application/account/controller/CmUserController.java
@@ -1,10 +1,13 @@
 package com.example.health_care_back.application.account.controller;
 
+import com.example.health_care_back.application.account.controller.dto.ConfirmPasswordUserDTO;
 import com.example.health_care_back.application.account.service.UserCmService;
 import com.example.health_care_back.application.common.response.CommonResponse;
 import com.example.health_care_back.application.vo.UserVO;
 import com.example.health_care_back.infra.config.security.user.LoginUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,5 +23,12 @@ public class CmUserController {
   public CommonResponse<UserVO> getUser(@PathVariable(value = "user-id") Long userId,
     @AuthenticationPrincipal LoginUser loginUser) {
     return CommonResponse.success(userCmService.getUser(loginUser, userId));
+  }
+
+  @PostMapping(value = "/{user-id}/confirm:password", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public CommonResponse<Boolean> confirmPassword(@PathVariable(value = "user-id") Long userId,
+                                                 @Valid @RequestBody ConfirmPasswordUserDTO dto,
+                                                 @AuthenticationPrincipal LoginUser loginUser) {
+    return CommonResponse.success(userCmService.confirmPassword(loginUser, userId, dto));
   }
 }
--- src/main/java/com/example/health_care_back/application/account/service/UserCmService.java
+++ src/main/java/com/example/health_care_back/application/account/service/UserCmService.java
@@ -1,5 +1,6 @@
 package com.example.health_care_back.application.account.service;
 
+import com.example.health_care_back.application.account.controller.dto.ConfirmPasswordUserDTO;
 import com.example.health_care_back.application.account.domain.User;
 import com.example.health_care_back.application.account.domain.code.UserStatus;
 import com.example.health_care_back.application.account.repository.UserRepository;
@@ -10,6 +11,7 @@ import com.example.health_care_back.application.common.exception.ResourceExcepti
 import com.example.health_care_back.application.vo.UserVO;
 import com.example.health_care_back.infra.config.security.user.LoginUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 @Service
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserCmService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserVO getUser(LoginUser loginUser, Long userId) {
         User user = userRepository.findByIdAndUserStatusIs(userId, UserStatus.ACTIVATED)
@@ -28,5 +31,16 @@ public class UserCmService {
         }
 
         return UserVO.valueOf(user);
+    }
+
+    public boolean confirmPassword(LoginUser loginUser, Long userId, ConfirmPasswordUserDTO dto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceException(ResourceExceptionCode.RESOURCE_NOT_FOUND));
+
+        if (!user.getId().equals(loginUser.getId())) {
+            throw new AuthException(AuthExceptionCode.NOT_AUTHORIZED);
+        }
+
+        return passwordEncoder.matches(dto.password(), user.getPassword());
     }
 }
