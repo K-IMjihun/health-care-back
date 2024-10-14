90835658ed3824d5f81e85fa879b908e93f6a5cf
K-IMjihun <kjh950901@gmail.com>
2024-10-14 13:05:33 +0900

    유저 조회기능 추가

--- src/main/java/com/example/health_care_back/application/account/controller/SyUserController.java
+++ src/main/java/com/example/health_care_back/application/account/controller/SyUserController.java
@@ -1,9 +1,13 @@
 package com.example.health_care_back.application.account.controller;
 
 import com.example.health_care_back.application.account.controller.dto.ChangeAuthorityDTO;
+import com.example.health_care_back.application.account.controller.dto.SearchUserDTO;
 import com.example.health_care_back.application.account.service.UserSyService;
+import com.example.health_care_back.application.common.response.CommonResponse;
+import com.example.health_care_back.application.vo.UserVO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,5 +24,12 @@ public class SyUserController {
     @PathVariable(value = "user-id") long userId,
     @Valid @RequestBody ChangeAuthorityDTO dto) {
     userSyService.changeAuthority(userId, dto);
+  }
+
+  // 유저 조회
+  @GetMapping
+  public CommonResponse<Page<UserVO>> getUsers(
+          @Valid SearchUserDTO dto) {
+    return CommonResponse.success(userSyService.getUsers(dto));
   }
 }
--- src/main/java/com/example/health_care_back/application/account/service/UserSyService.java
+++ src/main/java/com/example/health_care_back/application/account/service/UserSyService.java
@@ -1,13 +1,17 @@
 package com.example.health_care_back.application.account.service;
 
 import com.example.health_care_back.application.account.controller.dto.ChangeAuthorityDTO;
+import com.example.health_care_back.application.account.controller.dto.SearchUserDTO;
 import com.example.health_care_back.application.account.domain.User;
 import com.example.health_care_back.application.account.repository.UserRepository;
+import com.example.health_care_back.application.account.repository.param.SearchUserParam;
 import com.example.health_care_back.application.common.exception.ModifyPermissionException.ModifyPermissionExceptionCode;
 import com.example.health_care_back.application.common.exception.ResourceException;
 import com.example.health_care_back.application.common.exception.ResourceException.ResourceExceptionCode;
 import com.example.health_care_back.application.common.exception.ModifyPermissionException;
+import com.example.health_care_back.application.vo.UserVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,5 +39,9 @@ public class UserSyService {
 
         user.updateAuthority(dto.authorityType());
         userRepository.save(user);
+    }
+
+    public Page<UserVO> getUsers(SearchUserDTO dto) {
+        return userRepository.findAll(SearchUserParam.valueOf(dto), dto.toPageRequest());
     }
 }
