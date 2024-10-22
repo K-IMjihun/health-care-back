06792937729078281a88284893bc8ccd58f3be0b
K-IMjihun <kjh950901@gmail.com>
2024-10-22 19:36:19 +0900

    운동종목 조회

--- src/main/java/com/example/health_care_back/application/exercise/controller/ExerciseSyController.java
+++ src/main/java/com/example/health_care_back/application/exercise/controller/ExerciseSyController.java
@@ -2,16 +2,16 @@ package com.example.health_care_back.application.exercise.controller;
 
 import com.example.health_care_back.application.common.response.CommonResponse;
 import com.example.health_care_back.application.exercise.controller.dto.CreateExerciseDTO;
+import com.example.health_care_back.application.exercise.controller.dto.SearchExerciseDTO;
 import com.example.health_care_back.application.exercise.service.ExerciseSyService;
+import com.example.health_care_back.application.vo.ExerciseVO;
 import com.example.health_care_back.infra.config.security.user.LoginUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,10 +20,17 @@ public class ExerciseSyController {
 
     private final ExerciseSyService exerciseSyService;
 
+    // 운동종목 추가
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
     public CommonResponse<Void> createExercise(@Valid @RequestBody CreateExerciseDTO dto,
                                                @AuthenticationPrincipal LoginUser loginUser) {
         exerciseSyService.createExercise(loginUser, dto);
         return CommonResponse.success();
+    }
+
+    // 운동종목 조회
+    @GetMapping
+    public CommonResponse<Page<ExerciseVO>> getExercises(@Valid SearchExerciseDTO dto) {
+        return CommonResponse.success(exerciseSyService.getExercises(dto));
     }
 }
--- src/main/java/com/example/health_care_back/application/exercise/service/ExerciseSyService.java
+++ src/main/java/com/example/health_care_back/application/exercise/service/ExerciseSyService.java
@@ -1,14 +1,20 @@
 package com.example.health_care_back.application.exercise.service;
 
 import com.example.health_care_back.application.account.domain.User;
+import com.example.health_care_back.application.account.domain.code.AuthorityType;
 import com.example.health_care_back.application.account.domain.code.UserStatus;
 import com.example.health_care_back.application.account.repository.UserRepository;
 import com.example.health_care_back.application.common.exception.ResourceException;
 import com.example.health_care_back.application.common.exception.ResourceException.ResourceExceptionCode;
 import com.example.health_care_back.application.exercise.controller.dto.CreateExerciseDTO;
+import com.example.health_care_back.application.exercise.controller.dto.SearchExerciseDTO;
 import com.example.health_care_back.application.exercise.helper.ExerciseHelper;
+import com.example.health_care_back.application.exercise.repository.ExerciseRepository;
+import com.example.health_care_back.application.exercise.repository.param.SearchExerciseParam;
+import com.example.health_care_back.application.vo.ExerciseVO;
 import com.example.health_care_back.infra.config.security.user.LoginUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +25,7 @@ public class ExerciseSyService {
 
     private final UserRepository userRepository;
     private final ExerciseHelper exerciseHelper;
+    private final ExerciseRepository exerciseRepository;
 
     @Transactional
     public void createExercise(LoginUser loginUser, CreateExerciseDTO dto) {
@@ -27,4 +34,11 @@ public class ExerciseSyService {
 
         exerciseHelper.createExercise(user, dto);
     }
+
+    public Page<ExerciseVO> getExercises(SearchExerciseDTO dto) {
+        return exerciseRepository.findAll(
+                SearchExerciseParam.valueOf(dto, null, AuthorityType.SYSTEM),
+                dto.toPageRequest());
+    }
+
 }
